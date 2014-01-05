f20a1c3db2044b7f47f3fe50a33bfb04336fb90e
yakkuru201 <tonamimagic@gmail.com>
2014-01-05 12:52:12 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsubuyaki2nd::Application.config.secret_token = 'ded40bf622a661bdde20a467126aad0ac9469b8780a6d70358c259a7ad9ea8f89b386dcfcc3c8c7c8a2b4aeccebbe1610d186daba288701b9763f219d38352b3'
