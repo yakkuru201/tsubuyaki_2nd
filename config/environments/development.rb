d04eebb6ee31be7970f2e1d476e5852071a30cdd
yakkuru201 <tonamimagic@gmail.com>
2014-01-05 13:09:21 +0900

    メール認証の表示のための設定をした。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Tsubuyaki2nd::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # for devise development
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
