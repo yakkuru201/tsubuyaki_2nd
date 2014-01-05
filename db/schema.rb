d49eb538877b5e029209a258a907ce51d968afdb
yakkuru201 <tonamimagic@gmail.com>
2014-01-05 13:28:40 +0900

    マイグレーションした

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140105041032) do
+ActiveRecord::Schema.define(:version => 20140105042546) do
+
+  create_table "tweets", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
