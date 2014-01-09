668643b1a59f524051d4b5774540934656ffa7bb
yakkuru201 <tonamimagic@gmail.com>
2014-01-09 14:30:17 +0900

    tweetとuserを関連付けた

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,5 @@
 class Favorite < ActiveRecord::Base
+  attr_accessible :tweet_id, :user_id
+  belongs_to :user
+  belongs_to :tweet
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,7 @@ class Tweet < ActiveRecord::Base
   attr_accessible :content
   default_scope order('created_at DESC')
   belongs_to :user
+  has_many :favorites
   validates_associated :user
   validates :content, presence: true, length: { in: 1..140 }
 end
